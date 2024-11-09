46cb0e19f6bd88a97469c6d63b2494586413bacf
heavenlybilly <heavenlybilly@gmail.com>
2024-11-09 19:44:53 +0300

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,98 @@
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    jquery: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
+    '@vue/eslint-config-airbnb',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    moment: 'readonly',
+    defineProps: 'readonly',
+    defineEmits: 'readonly',
+    defineExpose: 'readonly',
+    withDefaults: 'readonly',
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    // TypeScript
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      {
+        functions: false,
+        classes: true,
+        variables: true,
+        enums: true,
+        typedefs: true,
+        ignoreTypeReferences: true,
+      },
+    ],
+
+    // Prettier
+    'prettier/prettier': 'error',
+
+    // Vue
+    'vue/v-on-event-hyphenation': 'error',
+    'vue/attributes-order': ['error', { alphabetical: true },],
+    'vuejs-accessibility/click-events-have-key-events': 'off',
+    'vuejs-accessibility/form-control-has-label': 'off',
+    'vuejs-accessibility/heading-has-content': 'off',
+    'vuejs-accessibility/iframe-has-title': 'off',
+    'vuejs-accessibility/interactive-supports-focus': 'off',
+    'vuejs-accessibility/label-has-for': 'off',
+    'vuejs-accessibility/media-has-caption': 'off',
+    'vuejs-accessibility/mouse-events-have-key-events': 'off',
+    'vuejs-accessibility/no-access-key': 'off',
+    'vuejs-accessibility/no-autofocus': 'off',
+    'vuejs-accessibility/no-distracting-elements': 'off',
+    'vuejs-accessibility/no-onchange': 'off',
+    'vuejs-accessibility/no-redundant-roles': 'off',
+    'vuejs-accessibility/role-has-required-aria-props': 'off',
+    'vuejs-accessibility/role-supports-aria-props': 'off',
+    'vuejs-accessibility/scope': 'off',
+    'vuejs-accessibility/tabindex-no-positive': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'eslint-import-resolver-custom-alias': {
+        alias: {
+          '@': './resources/ts',
+        },
+        extensions: ['.ts', '.vue'],
+        packages: ['packages/*'],
+      },
+    },
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import alias from '@rollup/plugin-alias'
+import vue from '@vitejs/plugin-vue2'
+import * as path from 'path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+export default defineConfig({
+  plugins: [
+    alias({
+      entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    }),
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'VueDatatables182',
+      fileName: (format) => `vue-datatables-182.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
